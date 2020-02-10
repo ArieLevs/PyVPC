db7c272c75d99b608d1a5c4d4161f487aa1ab657
jenkins <jenkins@nalkins.cloud>
2020-02-10 16:33:18 +0000

    update version to: 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ keywords = ['Networking', 'IPv4', 'IP', 'CIDR', 'VPC', 'AWS', 'Subnet', 'Range',
 
 setuptools.setup(
     name="pyvpc",
-    version="0.1.2",
+    version="0.1.3",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python AWS VPC CIDR available range finder with sub networks",
